d1a1a267b5623aeeae5c228e78d6861f6272bc86
swapnil1726@gmail.com <swapnil1726@gmail.com>
2024-01-11 00:09:00 +0530

    Auth with google

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:true,
-  
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'links.papareact.com',
+            port: '',
+            pathname: '/2i6',
+          },
+        ],
+      },
 }
 
 module.exports = nextConfig
